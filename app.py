21c0776b06e14f19d004735e964fb7c49555758b
“SAURAV <7thmail77@gmail.com>
2025-07-10 11:51:07 +0530

    Model Trainer Created

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from mlproject1.exception import CustomException
 from mlproject1.components.data_ingestion import DataIngestion
 from mlproject1.components.data_ingestion import DataIngestionConfig
 from mlproject1.components.data_transformation import DataTransformationConfig, Datatransformation
+from mlproject1.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 
 
@@ -16,8 +17,13 @@ if __name__ == "__main__":
          data_ingestion = DataIngestion()
          train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
 
+        # Data Transformation
          data_transformation = Datatransformation()
-         data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+         train_arr,test_arr,processor_path = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        # Model Training
+         model_trainer = ModelTrainer()
+         print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
      except Exception as e:
           logging.info("Custom Exception")
